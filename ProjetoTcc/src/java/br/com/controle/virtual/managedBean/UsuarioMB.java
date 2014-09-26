e90417d342c1943a8cc31453a04e05071b15c8cb
filipe_soares <filipe_soares@SNSOO1EP01VM112.saojose.sc.senai.br>
2014-09-25 21:00:39 -0300

    Configuração inicial

--- ProjetoTcc/src/java/br/com/controle/virtual/managedBean/UsuarioMB.java
+++ ProjetoTcc/src/java/br/com/controle/virtual/managedBean/UsuarioMB.java
@@ -1,10 +1,31 @@
-
 package br.com.controle.virtual.managedBean;
+
+import br.com.controle.virtual.entity.Usuario;
+import javax.faces.bean.ManagedBean;
 
 /**
  *
  * @author filipe_soares
  */
+@ManagedBean
 public class UsuarioMB {
-    
+
+    private Usuario bean;
+
+    public UsuarioMB() {
+        bean = new Usuario();
+    }
+
+    public Usuario getBean() {
+        return bean;
+    }
+
+    public void setBean(Usuario bean) {
+        this.bean = bean;
+    }
+
+    public Usuario save() {
+        bean.setNome(bean.getNome() + " Soares");
+        return bean;
+    }
 }
