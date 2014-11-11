4eb13bf2066a34972d96661bd3a8de187a6d6ddd
User <User@172.20.116.145>
2014-11-11 18:20:56 -0200

    Cadastrar exercicio;

--- ProjetoTcc/src/java/br/com/controle/virtual/entity/GrupoMuscular.java
+++ ProjetoTcc/src/java/br/com/controle/virtual/entity/GrupoMuscular.java
@@ -45,12 +45,34 @@ public class GrupoMuscular implements Serializable {
     public String toString() {
         return this.nome;
     }
-    
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        GrupoMuscular grupo = (GrupoMuscular) o;
+        if (id == null) {
+            if (grupo.id != null) {
+                return false;
+            }
+        } else if (!id.equals(grupo.id)) {
+            return false;
+        }
+        return true;
     }
 }
