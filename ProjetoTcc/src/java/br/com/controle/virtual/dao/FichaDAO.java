9b5dc6c32f40b4f3734b4e6c36d0fae986d1d014
User <User@172.20.116.204>
2014-12-01 21:10:31 -0200

    Criaçao da ficha (Execucao);

--- ProjetoTcc/src/java/br/com/controle/virtual/dao/FichaDAO.java
+++ ProjetoTcc/src/java/br/com/controle/virtual/dao/FichaDAO.java
@@ -3,7 +3,6 @@ package br.com.controle.virtual.dao;
 import br.com.controle.virtual.entity.Ficha;
 import java.util.List;
 import javax.persistence.Persistence;
-import org.hibernate.FetchMode;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
@@ -17,13 +16,13 @@ public class FichaDAO extends GenericDAO<Long, Ficha> {
         ficha = entityManager.getReference(Ficha.class, ficha.getId());
         super.delete(ficha);
     }
-    
+
     public List<Ficha> find(String nome) {
         List<Ficha> list;
         factory = Persistence.createEntityManagerFactory("controlevirtual");
         entityManager = factory.createEntityManager();
         Session session = (Session) entityManager.getDelegate();
-        list = session.createCriteria(Ficha.class).setFetchMode("usuario", FetchMode.JOIN).add(Restrictions.ilike("usuario.nome", nome, MatchMode.ANYWHERE)).list();
+        list = session.createCriteria(Ficha.class).add(Restrictions.ilike("usuario.nome", nome, MatchMode.ANYWHERE)).list();
         entityManager.close();
         factory.close();
         return list;
